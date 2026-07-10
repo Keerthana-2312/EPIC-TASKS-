c83342c42ad69da39880b25f470ec797c00daaf5
Keerthana-2312 <24bcs128@kgcas.com>
2026-07-10 16:28:47 +0530

    Number Problems

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+package Number_program;
+import java.util.Scanner;
+
+public class palindrome {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int n = sc.nextInt();
+	        int temp = n;
+	        int rev = 0;
+
+	        while (n > 0) {
+	            rev = rev * 10 + (n % 10);
+	            n = n / 10;
+	        }
+	        if (temp == rev)
+	            System.out.println("Palindrome");
+	        else
+	            System.out.println("Not Palindrome");
+	    }
+}  
+
