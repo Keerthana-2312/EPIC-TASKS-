c1a439b1fbfda75f9dbd3347c02c42b9ce2c6571
Keerthana-2312 <24bcs128@kgcas.com>
2026-07-10 16:23:41 +0530

    Number Problem

---
+++ Reverse String.java
@@ -0,0 +1,18 @@
+package Number_program;
+import java.util.Scanner;
+
+public class reverse_string {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int n = sc.nextInt();
+	        
+	        int rev = 0;
+	        while (n > 0) {
+	            rev = rev * 10 + (n % 10);
+	            n = n / 10;
+	        }
+	        System.out.println(rev);
+	    }
+	}
+    
